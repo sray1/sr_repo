3c3055f22e494ce8822f736f7e4ba812f4fcfc30
sray1 <sray12345678@gmail.com>
2015-12-19 14:04:18 -0800

    add more folders

---
+++ knn/cifar-10-batches-py/test1.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 31 14:34:08 2015
+
+@author: Shonket
+"""
+import numpy as np;
+import matplotlib.pyplot as plt;
+
+from cs231nlib.classifier import NearestNeighbor;
+from cs231nlib.utils import load_CIFAR10;
+from cs231nlib.utils import visualize_CIFAR;
+
+## load dataset
+
+Xtr, Ytr, Xte, Yte=load_CIFAR10("data/CIFAR10");
+
+print Xtr.shape[0];
+print Xtr.shape[1];
+print Xtr.shape[2];
+print Xtr.shape[3];
+
+## plot configuration
+
+plt.rcParams['figure.figsize']=(10.0, 8.0);
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+visualize_CIFAR(X_train=Xtr, y_train=Ytr, samples_per_class=10);
+
+
+
+## Testing for Nearest Neighbor Function
+
+nn=NearestNeighbor();
